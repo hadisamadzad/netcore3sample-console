6c1317ebe9893f11d6a614a77647bf6e16549a4c
Hadi Samadzad <h.samadzad@gmail.com>
2019-12-14 01:53:35 +0330

    First commit

---
+++ SampleConsole/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsole
+{
+    public class MyClass
+    {
+        public async Task MyAsyncMethod()
+        {
+            await doSomething();
+            Console.WriteLine("I thought this should come first");
+        }
+
+        public async Task doSomething()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("I should come second");
+        }
+    }
+    
+}
---
+++ SampleConsole/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.GetAvailableThreads(out int availableThreads, out int port);
+            Console.WriteLine(availableThreads);
+            Console.WriteLine(port);
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(2, 1);
+
+            Console.WriteLine(ThreadPool.ThreadCount);
+            Console.WriteLine(ThreadPool.PendingWorkItemCount);
+            Console.WriteLine(ThreadPool.CompletedWorkItemCount);
+            Console.ReadKey();
+
+            MyAsyncMethod();
+
+            Console.ReadKey();
+        }
+
+        public static async Task MyAsyncMethod()
+        {
+            await Task.Run(() => doSomething());
+            Console.WriteLine("I thought this should come first");
+            //await task;
+        }
+
+        public static async Task doSomething()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("I should come second");
+        }
+
+        public static IEnumerable<double> M2(double number)
+        {
+            var res = new List<double>();
+            for (int i = 1; i <= number; i++)
+            {
+                res.Add(Math.Pow(1.01, i));
+            }
+
+            return res;
+        }
+
+        public static IEnumerable<double> YM2(double number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                yield return Math.Pow(1.01, i);
+            }
+        }
+
+        //public static Task<IEnumerable<double>> M2(double number)
+        //{
+        //    var res = new List<double>();
+        //    var task = new Task<IEnumerable<double>>(() =>
+        //    {
+        //        for (int i = 1; i <= number; i++)
+        //        {
+        //            res.Add(Math.Pow(1.01, i));
+        //        }
+
+        //        return res;
+        //    });
+
+        //    return task;
+        //}
+
+        //public static Task<IEnumerable<double>> YM2(double number)
+        //{
+        //    var res = new List<double>();
+        //    var task = new Task<IEnumerable<double>>(() =>
+        //    {
+        //        for (int i = 1; i <= number; i++)
+        //        {
+        //            yield return Math.Pow(1.01, i);
+        //        }
+
+        //        return res;
+        //    });
+
+        //    return task;
+        //}
+    }
+}
